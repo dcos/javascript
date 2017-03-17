7db76bbf00a5eaaa423ba2c4eb09bc64c24bce67
Dmitriy Rozhkov <drozhkov@mesosphere.io>
2017-03-17 14:24:50 +0100

    BREAKING: do it our way

---
+++ index.js
@@ -0,0 +1,37 @@
+module.exports = {
+  "plugins": [
+    "compat", // Browser compatibility
+    "destructuring",
+    "jsx-max-len",
+    "prettier",
+    "react"
+  ],
+  extends: [
+    "airbnb"
+  ],
+  rules: {
+    "prettier": "warn",
+
+    "destructuring/in-methods-params": "error",
+    "compat/compat": "error",
+
+    // Ternary
+    "no-nested-ternary": "error",
+    "no-unneeded-ternary": "error",
+    "multiline-ternary": "off", // allow both multiline and single line
+
+    // Airbnb overrides
+    "react/jsx-filename-extension": "off", // Allow JSX in .js files
+    "no-mixed-operators": ["error", {
+      "groups": [
+        ["+", "-", "*", "/", "%", "**"],
+        ["&", "|", "^", "~", "<<", ">>", ">>>"],
+        ["==", "!=", "===", "!==", ">", ">=", "<", "<="],
+        ["&&", "||"],
+        ["in", "instanceof"]
+      ],
+      "allowSamePrecedence": true // We do allow the same precedence
+    }],
+
+  }
+};
